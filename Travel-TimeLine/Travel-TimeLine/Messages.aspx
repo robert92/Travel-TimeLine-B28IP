23feddcabb2433a0332892ba29621bd622a47f9c
stefanoancea <stef_oancea17@yahoo.com>
2014-04-11 17:00:58 +0300

    Vizualizare mesaje!

---
+++ Travel-TimeLine/Travel-TimeLine/Messages.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Messages.aspx.cs" Inherits="Travel_TimeLine.Messages" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="FeaturedContent" runat="server">
+     <section class="featured">
+        <div class="content-wrapper">
+            <% if(Page.Session["autentificat"] == null || (bool) Page.Session["autentificat"] == false) { %>
+        <h3>You are not logged in !</h3>
+
+    <asp:Table ID="Table1" runat="server">
+        <asp:TableRow ID="TableRow1" runat="server">
+            <asp:TableCell ID="TableCell1" runat="server">
+                Username:
+            </asp:TableCell>
+            <asp:TableCell ID="TableCell2" runat="server">
+                <asp:TextBox ID="username" runat="server"></asp:TextBox> 
+            </asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow ID="TableRow2" runat="server">
+            <asp:TableCell ID="TableCell3" runat="server">
+                Password:
+            </asp:TableCell>
+            <asp:TableCell ID="TableCell4" runat="server">
+                <asp:TextBox ID="password" runat="server"></asp:TextBox> 
+            </asp:TableCell>
+        </asp:TableRow>
+        <asp:TableRow ID="TableRow3" runat="server">
+            <asp:TableCell ID="TableCell5" runat="server">
+            </asp:TableCell>
+            <asp:TableCell ID="TableCell6" runat="server">
+                <asp:Button ID="Button1" runat="server" Text="Log In" OnClick="logIn"/>
+            </asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+    <h3><asp:Literal ID="Literal1" runat="server" Text="Username/password invalid!" Visible="false"></asp:Literal></h3>
+
+    <h3>Or join now!</h3>
+        <asp:Button ID="Button2" runat="server" Text="Register" OnClick="register"/>
+    <% } else { %>
+
+              <p>
+        MY MESSAGES</p>
+    <asp:Panel ID="Panel1" runat="server">
+        Unread messages:<br />
+        <br />
+    </asp:Panel>
+    <asp:Panel ID="Panel2" runat="server">
+        Read messages:<br />
+        <br />
+    </asp:Panel>
+        
+    <% } %>
+        </div>
+    </section>
+</asp:Content>
+
+
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" runat="server">
+  
+</asp:Content>
---
+++ Travel-TimeLine/Travel-TimeLine/Messages.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Travel_TimeLine
+{
+    public partial class Messages : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection sqlSource = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Travel-TimeLine-Database.mdf;Integrated Security=True");
+            sqlSource.Open();
+            SqlCommand sqlCommand;
+            SqlDataReader read;
+            sqlCommand = new SqlCommand("select * from Messages join Messageslink on Messages.message_id=Messageslink.message_id where Messageslink.receiver='" + (String)Page.Session["username"] + "' and Messages.status=1", sqlSource);
+            read = sqlCommand.ExecuteReader();
+            SqlConnection sqlSource2 = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Travel-TimeLine-Database.mdf;Integrated Security=True");
+            sqlSource2.Open();
+
+            if (read.HasRows)
+            {
+                while (read.Read())
+                {
+                    SqlCommand sqlCommand2 = new SqlCommand("select * from Users where user_name='" + read.GetString(5) + "'", sqlSource2);
+                    SqlDataReader read2 = sqlCommand2.ExecuteReader();
+                    String cale = "";
+
+                    if (read2.HasRows)
+                    {
+                        read2.Read();
+                        cale = read2.GetString(8);
+
+                    }
+                    read2.Close();
+                    TextBox tbt = new TextBox();
+                    tbt.Text = "Title: " + read.GetString(1);
+                    Panel2.Controls.Add(tbt);
+                    Image img = new Image();
+                    img.ImageUrl = cale;
+                    Panel2.Controls.Add(img);
+                    TextBox tb = new TextBox();
+                    tb.Text = "Content: " + read.GetString(2);
+                    Panel2.Controls.Add(tb);
+
+                }
+
+            }
+            read.Close();
+
+
+            sqlCommand = new SqlCommand("select * from Messages join Messageslink on Messages.message_id=Messageslink.message_id where Messageslink.receiver='" + (String)Page.Session["username"] + "' and Messages.status=0", sqlSource);            
+            read = sqlCommand.ExecuteReader();
+            if (read.HasRows)
+            {
+                while (read.Read())
+                {
+                    SqlCommand sqlCommand2 = new SqlCommand("select * from Users where user_name='" + read.GetString(5) + "'", sqlSource2);
+                    SqlDataReader read2 = sqlCommand2.ExecuteReader();
+                    String cale = "";
+                    
+                    if (read2.HasRows)
+                    {
+                        read2.Read();
+                        cale = read2.GetString(8);
+
+                    }
+                    read2.Close();
+               TextBox tbt = new TextBox();
+              tbt.Text ="Title: " + read.GetString(1);
+              Panel1.Controls.Add(tbt);     
+                    Image img = new Image();
+                    img.ImageUrl = cale;
+                    Panel1.Controls.Add(img);
+                    TextBox tb = new TextBox();
+                    tb.Text ="Content: " + read.GetString(2);
+                    Panel1.Controls.Add(tb);
+
+                    sqlCommand2 = new SqlCommand("update Messages set status =1 where message_id= " + read.GetInt32(0), sqlSource2);
+                    sqlCommand2.ExecuteNonQuery();
+                }
+                    
+            }
+            read.Close();
+
+
+            
+
+
+            sqlSource2.Close();
+            sqlSource.Close();
+        }
+        protected void logIn(object sender, EventArgs e)
+        {
+            SqlConnection sqlSource = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Travel-TimeLine-Database.mdf;Integrated Security=True");
+            SqlCommand sqlCommand = new SqlCommand("select * from users where user_name='" + username.Text + "' and password='" + password.Text + "'", sqlSource);
+            sqlSource.Open();
+            SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            if (sqlReader.HasRows == true)
+            {
+                Literal1.Visible = false;
+
+                sqlReader.Read();
+
+                Page.Session["autentificat"] = true;
+                Page.Session["username"] = sqlReader.GetString(1);
+
+                HttpContext.Current.Response.Redirect("Messages.aspx");
+            }
+            else
+            {
+                Literal1.Visible = true;
+            }
+            sqlSource.Close();
+        }
+
+        protected void register(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Redirect("Inregistrare.aspx");
+        }
+    }
+}
